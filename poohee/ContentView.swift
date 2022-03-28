c99715987bb367da52991c88af1b9c57657f4a80
tottiliyt <tottiliyt@gmail.com>
2022-03-27 23:15:41 -0400

    Initial Commit

---
+++ poohee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  poohee
+//
+//  Created by Yuntao Li on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ poohee/pooheeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pooheeApp.swift
+//  poohee
+//
+//  Created by Yuntao Li on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct pooheeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
